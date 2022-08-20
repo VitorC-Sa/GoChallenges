865efab19d93c5c68be3b9906856347a415bdec9
VCarlosSoares <vitorcarlos2014soares@gmail.com>
2022-08-19 21:43:47 -0300

    feat: validateBraces challenges + new test case

--- codewars/validateBraces/validateBraces.go
+++ codewars/validateBraces/validateBraces.go
@@ -1,6 +1,44 @@
 package validatebraces
 
-func ValidBraces(str string) bool {
+import (
+	"strings"
+)
 
-	return false
+/* Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+What is considered Valid?
+A string of braces is considered valid if all braces are matched with the correct brace. */
+
+func MySolution(str string) bool {
+	var indexBackup []int
+	s := 0
+	sBracesIni := "([{"
+	sBracesEnd := ")]}"
+
+	if isFirstIndexValid := strings.IndexByte(sBracesIni, str[0]); isFirstIndexValid == -1 {
+		return false
+	}
+
+	for _, v := range str {
+		if index := strings.IndexRune(sBracesIni, v); index != -1 { //get index of v if match with Ini Braces
+			indexBackup = append(indexBackup, index)
+			s++
+		}
+		if index := strings.IndexRune(sBracesEnd, v); index != -1 { //get index of v if match with End Braces
+			l := len(indexBackup)
+			if index != indexBackup[l-1] { //compare last End Brace with last Ini Brace
+				return false
+			}
+			indexBackup = append(indexBackup, indexBackup[:l-1]...) //delete last Ini Brace index from indexBackup
+			s--
+		}
+	}
+
+	if s != 0 {
+		return false
+	}
+
+	return true
 }
--- codewars/validateBraces/validateBraces_test.go
+++ codewars/validateBraces/validateBraces_test.go
@@ -1,6 +1,8 @@
 package validatebraces
 
-import "testing"
+import (
+	"testing"
+)
 
 type args struct {
 	str string
@@ -14,20 +16,24 @@ type tests struct {
 
 func getTestCase() []tests {
 	return []tests{
+		{name: "t0", args: args{str: "()"}, want: true},
 		{name: "t1", args: args{str: "(){}[]"}, want: true},
 		{name: "t2", args: args{str: "([{}])"}, want: true},
 		{name: "t3", args: args{str: "(}"}, want: false},
 		{name: "t4", args: args{str: "[(])"}, want: false},
 		{name: "t5", args: args{str: "[({)](]"}, want: false},
+		{name: "t6", args: args{str: "(("}, want: false},
+		{name: "t7", args: args{str: "(((({{}})[]))"}, want: false},
+		{name: "t8", args: args{str: "()({{}}"}, want: false},
 	}
 }
 
-func TestValidBraces(t *testing.T) {
+func TestMySolution(t *testing.T) {
 	tests := getTestCase()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ValidBraces(tt.args.str); got != tt.want {
-				t.Errorf("ValidBraces() = %v, want %v", got, tt.want)
+			if got := MySolution(tt.args.str); got != tt.want {
+				t.Errorf("MySolution() = %v, want %v", got, tt.want)
 			}
 		})
 	}
