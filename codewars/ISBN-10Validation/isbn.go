9e6d827b2d01406c8f4406063f2ca98e6ab8782c
VCarlosSoares <vitorcarlos2014soares@gmail.com>
2022-11-01 22:41:53 -0300

    added ISBN challenge details

--- codewars/ISBN-10Validation/isbn.go
+++ codewars/ISBN-10Validation/isbn.go
@@ -1,5 +1,11 @@
 package isbn10validation
 
+/*
+	ISBN-10 identifiers are ten digits long. The first nine characters are digits 0-9. The last digit can be 0-9 or X, to indicate a value of 10.
+
+	An ISBN-10 number is valid if the sum of the digits multiplied by their position modulo 11 equals zero.
+*/
+
 import (
 	"strconv"
 	"strings"
