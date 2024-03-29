de51a825b69291366818357400de5bca786e2adb
VitorC-Sa <vitorcarlos2014soares@gmail.com>
2024-03-29 12:53:30 -0300

    challenge: find the odd integer

---
+++ codewars/findTheOddInt/findTheOddInt.go
@@ -0,0 +1,28 @@
+package findtheoddint
+
+/*
+	Given an array of integers, find the one that appears an odd number of times.
+	There will always be only one integer that appears an odd number of times.
+	Examples
+	[7] should return 7, because it occurs 1 time (which is odd).
+	[0] should return 0, because it occurs 1 time (which is odd).
+	[1,1,2] should return 2, because it occurs 1 time (which is odd).
+	[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+	[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+	Kata Link: https://www.codewars.com/kata/54da5a58ea159efa38000836/train/go
+*/
+func FindOdd(seq []int) int {
+	nMap := map[int]int{}
+
+	for _, v := range seq {
+		nMap[v]++
+	}
+
+	for k, v := range nMap {
+		if v%2 == 1 {
+			return k
+		}
+	}
+	return 0
+}
